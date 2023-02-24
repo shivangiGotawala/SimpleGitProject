7fbaa864eebfb27d619e7d72fe16e2a380ac944d
shivangiGotawala <shivangimarwadi3375@gmail.com>
2023-02-24 14:13:07 -0800

    commit main modified file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This project is just for the github commands practise");
+        System.out.println("PLease use this for practise only");
+        System.out.println("All the best");
     }
 }
